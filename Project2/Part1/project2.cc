bb5af2fcdcc0fb518265a418b0416a910c7cb163
Matthew <mc067415@ohio.edu>
2019-03-06 14:03:29 -0500

    project working except preallocate blocks not showing 0

--- Project2/Part1/project2.cc
+++ Project2/Part1/project2.cc
@@ -3,9 +3,14 @@
 #include <cstring>
 #include <string>
 #include <cmath>
+#include <algorithm> 
 #include "ext2fs.h"
 
 using namespace std;
+
+void to_block(int block, ifstream& ext2, ext2_super_block &superblock){   
+    ext2.seekg((1024 >> superblock.s_log_block_size) * block);
+}
 
 
 int main(int argc, char **argv){
@@ -36,8 +41,6 @@ int main(int argc, char **argv){
         return(-1);
     }    
 
-    ext2_group_desc GroupDec;
-
     cout << "Superblock magic number:" << hex <<superblock.s_magic << endl
     << "Inodes Count: " << dec << superblock.s_inodes_count << endl
     << "Block Count: " << superblock.s_blocks_count <<endl
@@ -50,14 +53,43 @@ int main(int argc, char **argv){
     << "Fragments Per Group: "<< superblock.s_frags_per_group << endl
     << "Inodes Per Group: " << superblock.s_inodes_per_group << endl 
     << "Number of Block Groups: " << ceil((float)superblock.s_blocks_count / (float)superblock.s_blocks_per_group) << endl
-    << "Number of Pre-Allocate Blocks: " << superblock.s_prealloc_blocks << endl
-    << "Number of Pre-Allocate Directory Blocks: " << superblock.s_prealloc_dir_blocks << endl
-    << "Inode size: "<< superblock.s_inode_size << endl; 
+    << "Number of Pre-Allocate Blocks: " << max((__u8)0, superblock.s_prealloc_blocks) << endl
+    << "Number of Pre-Allocate Directory Blocks: " << max((__u8)0,superblock.s_prealloc_dir_blocks) << endl
+    << "Inode size: "<< max((int)superblock.s_inode_size,(int)sizeof(struct ext2_inode)) << endl; 
 
     
+    if(superblock.s_log_block_size == 0 ){ // the block size is 1024 bytes so the group descriptor table starts at logic block 2  
+        to_block(2,ext2,superblock);
+    }else{ //starts at block 1 otherwize
+        to_block(1,ext2,superblock);
+    }
+    // stream pointer now at the group descriptor 
 
+    int NumGroupDecs = 1 + (superblock.s_blocks_count-1) / superblock.s_blocks_per_group; // **********better understand this formula*****
 
+    ext2_group_desc GroupDec[NumGroupDecs];
+    for (int i=0;i<NumGroupDecs; i++){
+        ext2.read((char*)&GroupDec[i],sizeof(GroupDec[i]));
+    }
+
+    cout << endl << endl;
+
+    for (int j=0; j<NumGroupDecs;j++){
+        cout << "Information for Block Group " << j << endl
+        << "-------------------------------------------------------------------------------" << endl
+        << "Group Range: " << (superblock.s_blocks_per_group*j)+1 << " to "<< min(superblock.s_blocks_count-1 , (superblock.s_blocks_per_group*(j+1))) << endl
+        << "Block Bitmap Block at: " << GroupDec[j].bg_block_bitmap << endl
+        << "Inodes Bitmap Block at: " << GroupDec[j].bg_inode_bitmap << endl
+        << "Free Blocks Count: " << GroupDec[j].bg_free_blocks_count << endl
+        << "Free Inodes Count: " << GroupDec[j].bg_free_inodes_count << endl
+        << "Used Directories Count: " << GroupDec[j].bg_used_dirs_count << endl
+        << "-------------------------------------------------------------------------------" << endl << endl;
+    }
+    
+    
 
     return 0;
+
+
 
 } 
